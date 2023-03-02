2331d7aab02e9340a503dc48e7115a75b6c15971
GyuriKim <artgr2062@konkuk.ac.kr>
2023-03-02 14:46:51 +0900

    React로 Form 구현

---
+++ src/components/meetups/NewMeetupForm.js
@@ -0,0 +1,62 @@
+import { useRef } from "react";
+import Card from "../ui/Card";
+import classes from "./NewMeetupForm.module.css";
+
+function NewMeetupForm() {
+  // 강의에서는 null을 넣진 않았다.
+  const titleInputRef = useRef(null);
+  const imageInputRef = useRef(null);
+  const addressInputRef = useRef(null);
+  const descriptionInputRef = useRef(null);
+
+  function onSubmit(event) {
+    event.preventDefault();
+
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    /* 이렇게 새롭게 Object를 우리가 직접 만들어주기 때문에,
+    input에 nmae 속성이 따로 필요 없는 듯 */
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription,
+    };
+  }
+
+  return (
+    <Card>
+      <form className={classes.form} onSubmit={onSubmit}>
+        <div className={classes.control}>
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" required id="title" ref={titleInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="image">Meetup Image</label>
+          <input type="url" required id="image" ref={imageInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="address">Address</label>
+          <input type="text" required id="address" ref={addressInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="description">Description</label>
+          <textarea
+            id="description"
+            required
+            rows="5"
+            ref={descriptionInputRef}
+          />
+        </div>
+        <div className={classes.actions}>
+          <button>Add Meetup</button>
+        </div>
+      </form>
+    </Card>
+  );
+}
+
+export default NewMeetupForm;
