3f8f3e8e25c23da2dc79b665187f4d5c6ee4b1ba
GyuriKim <artgr2062@konkuk.ac.kr>
2023-03-05 14:01:19 +0900

    Firebase 연결 및 HTTP POST 요청 코드 작성 (2)
    - axios로 fetch 구문 변경
    - await/async 구문으로 코드 작성
    - replace: true 속성 사용해, 이전 버튼 눌렀을 때 history 접근 없애기
    - firebase 실시간 데이터 베이스 연결 유지

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ## 리액트 최종 복습
 
 - [x] 모달창 구현
-- [x] custom component 만들기(map으로 데이터를 JSX 컴포넌트로 렌더링, children prop 사용)
+- [x] custom component 만들기 (map으로 데이터를 JSX 컴포넌트로 렌더링, children prop 사용)
 - [x] React로 Form 구현 (useRef 사용)
+- [x] Firebase 연결 및 HTTP POST 요청 코드 작성 (useNavigate, axios, fetch, replace, firebase)
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,18 +1,33 @@
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetups() {
   const navigate = useNavigate();
-  const addMeetupHandler = (meetupData) => {
-    fetch(`${process.env.REACT_APP_DATABASE_BASE_URL}/meetups.json`, {
-      method: "POST",
-      body: JSON.stringify(meetupData),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }).then(() => {
-      navigate("/");
-    });
+  /* fetch, then 버전 */
+  // const addMeetupHandler = (meetupData) => {
+  //   fetch(`${process.env.REACT_APP_DATABASE_BASE_URL}/meetups.json`, {
+  //     method: "POST",
+  //     body: JSON.stringify(meetupData),
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   }).then(() => {
+  //     navigate("/");
+  //   });
+  // };
+
+  /* axios, navigate(replace) 버전 */
+  const addMeetupHandler = async (meetupData) => {
+    try {
+      await axios.post(
+        `${process.env.REACT_APP_DATABASE_BASE_URL}/meetups.json`,
+        meetupData
+      );
+      navigate("/", { replace: true });
+    } catch (error) {
+      console.log("Error:", error.response.data);
+    }
   };
   return (
     <section>
